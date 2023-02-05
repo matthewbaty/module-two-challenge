be5cd86296cdc0a5fa978625fb788e00587c2f1a
Matthew Baty <mattbaty@outlook.com>
2023-02-04 19:28:03 -0500

    2x2 grid setup and contact me section start

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// const navLinks = document.querySelectorAll('header');
+
+// navLinks.forEach(link => {
+//   link.addEventListener('click', e => {
+//     e.preventDefault();
+//     const sectionId = link.getAttribute('href');
+//     document.querySelector(sectionId).scrollIntoView({
+//       behavior: 'smooth'
+//     });
+//   });
+// });
